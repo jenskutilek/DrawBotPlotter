9293c009ea9648c3d5aa75467d2c4cdeaada8890
Jens Kutilek <webmail@kutilek.de>
2016-06-01 17:42:36 +0200

    Add stuff to demo script

--- scripts/Simple Demo.py
+++ scripts/Simple Demo.py
@@ -7,7 +7,6 @@ addHPGLContext()
 size(1145, 782)
 fill(None)
 strokeWidth(1)
-stroke(0)
 stroke(1, 0, 0)
 rect(0, 0, 72, 72)
 stroke(0, 0, 0)
@@ -16,16 +15,30 @@ save()
 translate(width()/2, height()/2)
 stroke(0, 0, 1)
 strokeWidth(2)
-rect(-36, -36, 72, 72)
+if True:
+    w = 2
+    for i in range(10):
+        rect(-36, -36, 72, 72)
+        #fill(1, 1, 1, .6)
+        rect(180, 0, 72, 72)
+        rotate(10)
+        scale(1.2)
+        w /= 1.2
+        strokeWidth(w)
 restore()
 oval(537, 355, 72, 72)
 d = 300
-#oval((width()-d)/2, (height()-d)/2, d, d)
+oval((width()-d)/2, (height()-d)/2, d, d)
+translate(72, 72)
 path = BezierPath()
 path.moveTo((0, 0))
 path.curveTo((55, 0), (100, 45), (100, 100))
 path.endPath()
 drawPath(path)
+
+#translate(200, 200)
+#rect(-100, -100, 72, 72)
+
 saveImage("~/Desktop/HPGLContext.hpgl")
 
 # To test the result, draw the HPGL file in DrawBot
