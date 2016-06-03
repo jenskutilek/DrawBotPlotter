917fb304ec3dd86ccb23c2f99619a246d76c24f3
Jens Kutilek <webmail@kutilek.de>
2016-06-03 12:04:15 +0200

    Change parameters for curve length and segment size

--- scripts/Simple Demo.py
+++ scripts/Simple Demo.py
@@ -1,7 +1,12 @@
 from drawbotPlotter.HPGLContext import addHPGLContext
 from drawbotPlotter.HPGLDraw import HPGLDraw
+from drawBot.context import allContexts
 
 addHPGLContext()
+
+hc = allContexts[-1]
+hc.min_segment_units = 20
+hc.max_curve_steps = 50
 
 #size("A3Landscape") # 1190 * 842
 size(1145, 782)
@@ -15,7 +20,7 @@ save()
 translate(width()/2, height()/2)
 stroke(0, 0, 1)
 strokeWidth(2)
-if True:
+if False:
     w = 2
     for i in range(10):
         rect(-36, -36, 72, 72)
@@ -26,20 +31,42 @@ if True:
         w /= 1.2
         strokeWidth(w)
 restore()
+stroke(0, 0, 1)
 oval(537, 355, 72, 72)
 d = 300
-oval((width()-d)/2, (height()-d)/2, d, d)
+translate((width()-d)/2, (height()-d)/2)
+translate(-d, 0)
+stroke(0, 1, 0)
+oval(0, 0, d, d)
+    
+if False:
+    save()
+    d = 300
+    translate((width()-d)/2, (height()-d)/2)
+    translate(-d, 0)
+    oval(0, 0, d, d)
+    translate(d, 0)
+    save()
+    scale(0.5)
+    oval(0, 0, d*2, d*2)
+    restore()
+    translate(d, 0)
+    scale(2)
+    oval(0, 0, d/2, d/2)
+    restore()
 translate(72, 72)
-path = BezierPath()
-path.moveTo((0, 0))
-path.curveTo((55, 0), (100, 45), (100, 100))
-path.endPath()
-drawPath(path)
+if False:
+    path = BezierPath()
+    path.moveTo((0, 0))
+    path.curveTo((55, 0), (100, 45), (100, 100))
+    path.endPath()
+    drawPath(path)
 
 #translate(200, 200)
 #rect(-100, -100, 72, 72)
 
 saveImage("~/Desktop/HPGLContext.hpgl")
+#saveImage("~/Desktop/HPGLContext.pdf")
 
 # To test the result, draw the HPGL file in DrawBot
 
